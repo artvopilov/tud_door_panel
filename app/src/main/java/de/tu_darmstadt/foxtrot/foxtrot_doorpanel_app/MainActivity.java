b93b24e3893966fd33bb814ce4260d6de6d08c57
Florian Schunk <florian.schunk@rwth-aachen.de>
2018-11-20 21:24:15 +0100

    added font awsome icons(CC-BY), weekview lib (apache license) and activity to make appointments

--- app/src/main/java/de/tu_darmstadt/foxtrot/foxtrot_doorpanel_app/MainActivity.java
+++ app/src/main/java/de/tu_darmstadt/foxtrot/foxtrot_doorpanel_app/MainActivity.java
@@ -2,6 +2,9 @@ package de.tu_darmstadt.foxtrot.foxtrot_doorpanel_app;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageButton calendarButton = findViewById(R.id.b7);
+        calendarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_make_appointment);
+            }
+        });
     }
 }
