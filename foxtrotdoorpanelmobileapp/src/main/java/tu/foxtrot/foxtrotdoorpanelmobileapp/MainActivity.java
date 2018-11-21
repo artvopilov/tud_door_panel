3a430339bdbd502fbd8245c6c76d3297230c155f
mister025 <mister.0025@yandex.ru>
2018-11-21 21:53:58 +0100

    calendar-mockup
    Calendar mockup with tabs is ready. version 1.0

--- foxtrotdoorpanelmobileapp/src/main/java/tu/foxtrot/foxtrotdoorpanelmobileapp/MainActivity.java
+++ foxtrotdoorpanelmobileapp/src/main/java/tu/foxtrot/foxtrotdoorpanelmobileapp/MainActivity.java
@@ -1,13 +1,27 @@
 package tu.foxtrot.foxtrotdoorpanelmobileapp;
 
+import android.content.Intent;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button calendarButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        calendarButton = (Button) findViewById(R.id.button2);
+        calendarButton.setOnClickListener(this::openCalendar);
+    }
+
+    public void openCalendar(View view) {
+        Intent intent = new Intent(this, CalendarActivity.class);
+        startActivity(intent);
     }
 }
