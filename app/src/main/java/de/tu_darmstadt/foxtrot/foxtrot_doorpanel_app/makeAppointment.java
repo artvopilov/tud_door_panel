a2276af3d6d65d2039e358042543c4e411d2de11
Florian Schunk <florian.schunk@rwth-aachen.de>
2018-11-22 21:55:12 +0100

    added activity to make an appointment

--- app/src/main/java/de/tu_darmstadt/foxtrot/foxtrot_doorpanel_app/makeAppointment.java
+++ app/src/main/java/de/tu_darmstadt/foxtrot/foxtrot_doorpanel_app/makeAppointment.java
@@ -1,16 +1,30 @@
 package de.tu_darmstadt.foxtrot.foxtrot_doorpanel_app;
 
+import android.content.Context;
+import android.graphics.RectF;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
+
+import com.alamkanak.weekview.MonthLoader;
+import com.alamkanak.weekview.WeekView;
+import com.alamkanak.weekview.WeekViewEvent;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 public class makeAppointment extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_appointment);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -25,6 +39,36 @@ public class makeAppointment extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Get a reference for the week view in the layout.
+        WeekView mWeekView = (WeekView) findViewById(R.id.weekView);
+
+        // Set an action when any event is clicked.
+        mWeekView.setOnEventClickListener(new WeekView.EventClickListener() {
+            @Override
+            public void onEventClick(WeekViewEvent event, RectF eventRect) {
+
+            }
+        });
+
+        // The week view has infinite scrolling horizontally. We have to provide the events of a
+        // month every time the month changes on the week view.
+        mWeekView.setMonthChangeListener(new MonthLoader.MonthChangeListener() {
+            @Override
+            public List<WeekViewEvent> onMonthChange(int newYear, int newMonth) {
+                return new ArrayList<WeekViewEvent>();
+            }
+        });
+
+        // Set long press listener for events.
+        mWeekView.setEventLongPressListener(new WeekView.EventLongPressListener() {
+            @Override
+            public void onEventLongPress(WeekViewEvent event, RectF eventRect) {
+
+            }
+        });
+
+
     }
 
 }
