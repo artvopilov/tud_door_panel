1461458ff69d2399c737404214de19c120df2314
Florian Schunk <florian.schunk@rwth-aachen.de>
2018-11-22 21:55:12 +0100

    added activity to make an appointment

--- app/src/main/java/de/tu_darmstadt/foxtrot/foxtrot_doorpanel_app/MainActivity.java
+++ app/src/main/java/de/tu_darmstadt/foxtrot/foxtrot_doorpanel_app/MainActivity.java
@@ -1,5 +1,6 @@
 package de.tu_darmstadt.foxtrot.foxtrot_doorpanel_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -16,7 +17,8 @@ public class MainActivity extends AppCompatActivity {
         calendarButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                setContentView(R.layout.activity_make_appointment);
+                Intent intent = new Intent(v.getContext(), makeAppointment.class);
+                startActivity(intent);
             }
         });
     }
