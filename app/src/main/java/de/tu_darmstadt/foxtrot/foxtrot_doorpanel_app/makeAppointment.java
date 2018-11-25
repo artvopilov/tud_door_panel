e836e10693e879136599510b1b7ab2fe4e4cf1e2
Florian Schunk <florian.schunk@rwth-aachen.de>
2018-11-25 22:44:52 +0100

    polished make appointment

--- app/src/main/java/de/tu_darmstadt/foxtrot/foxtrot_doorpanel_app/makeAppointment.java
+++ app/src/main/java/de/tu_darmstadt/foxtrot/foxtrot_doorpanel_app/makeAppointment.java
@@ -1,15 +1,19 @@
 package de.tu_darmstadt.foxtrot.foxtrot_doorpanel_app;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
 import android.graphics.RectF;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 import com.alamkanak.weekview.MonthLoader;
 import com.alamkanak.weekview.WeekView;
@@ -21,6 +25,14 @@ import java.util.List;
 
 public class makeAppointment extends AppCompatActivity {
 
+    private WeekViewEvent activeEvent = null;
+    private int slotColor = 0x77770000;
+    private int activeSlotColor = 0xFF550000;
+    private List<WeekViewEvent> events;
+    private  boolean nameEdited = false;
+    private  boolean mailEdited = false;
+    private  boolean messageEdited = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -30,24 +42,114 @@ public class makeAppointment extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        events = new ArrayList<WeekViewEvent>();
+        WeekViewEvent testEvent = new WeekViewEvent();
+        testEvent.setStartTime(Calendar.getInstance());
+        testEvent.setName("free slot");
+        Calendar end = Calendar.getInstance();
+        end.add(Calendar.HOUR,1);
+        testEvent.setEndTime(end);
+        testEvent.setColor(getResources().getColor(R.color.colorSlot));
+        events.add(testEvent);
+        WeekViewEvent testEvent2 = new WeekViewEvent();
+        Calendar start2 = Calendar.getInstance();
+        start2.add(Calendar.HOUR,2);
+        testEvent2.setStartTime(start2);
+        testEvent2.setName("free slot");
+        Calendar end2 = Calendar.getInstance();
+        end2.add(Calendar.HOUR,3);
+        testEvent2.setEndTime(end2);
+        testEvent2.setColor(getResources().getColor(R.color.colorSlot));
+        events.add(testEvent2);
+
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                finish();
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        fab.setEnabled(false);
+
         // Get a reference for the week view in the layout.
-        WeekView mWeekView = (WeekView) findViewById(R.id.weekView);
+        final WeekView mWeekView = (WeekView) findViewById(R.id.weekView);
 
         // Set an action when any event is clicked.
         mWeekView.setOnEventClickListener(new WeekView.EventClickListener() {
             @Override
             public void onEventClick(WeekViewEvent event, RectF eventRect) {
 
+                Log.v("DoorPanel", "slot clicked" );
+
+                if (! (activeEvent == null)) {
+                    activeEvent.setColor(getResources().getColor(R.color.colorSlot));
+                }
+                activeEvent = event;
+                activeEvent.setColor(getResources().getColor(R.color.colorSlotActive) );
+
+                if (nameEdited && mailEdited && messageEdited){
+                    fab.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.colorSlotActive)));
+                    fab.setEnabled(true);
+                }
+
+
+
+            }
+        });
+
+        ((EditText) findViewById(R.id.editName)).addTextChangedListener(new TextWatcher() {
+            public void afterTextChanged(Editable s) {
+                nameEdited = true;
+                if (activeEvent != null && mailEdited && messageEdited){
+                    fab.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.colorSlotActive)));
+                    fab.setEnabled(true);
+                }
+            }
+
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+            }
+
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+            }
+        });
+
+        ((EditText) findViewById(R.id.editEMail)).addTextChangedListener(new TextWatcher() {
+            public void afterTextChanged(Editable s) {
+                mailEdited = true;
+                if (activeEvent != null && nameEdited && messageEdited){
+                    fab.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.colorSlotActive)));
+                    fab.setEnabled(true);
+                }
+            }
+
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+            }
+
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+            }
+        });
+
+        ((EditText) findViewById(R.id.editMessage)).addTextChangedListener(new TextWatcher() {
+            public void afterTextChanged(Editable s) {
+                nameEdited = true;
+                if (activeEvent != null && mailEdited && nameEdited){
+                    fab.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.colorSlotActive)));
+                    fab.setEnabled(true);
+                }
+            }
+
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+            }
+
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
             }
         });
 
@@ -56,7 +158,8 @@ public class makeAppointment extends AppCompatActivity {
         mWeekView.setMonthChangeListener(new MonthLoader.MonthChangeListener() {
             @Override
             public List<WeekViewEvent> onMonthChange(int newYear, int newMonth) {
-                return new ArrayList<WeekViewEvent>();
+
+                return events;
             }
         });
 
