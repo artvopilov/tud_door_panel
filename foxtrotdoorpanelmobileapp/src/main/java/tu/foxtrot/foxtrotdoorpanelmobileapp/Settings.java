233392f775c8b84bc4b2d959e75ded668b1859dc
Javier Ochoa <jochoaserna@hotmail.com>
2019-01-20 21:03:32 +0100

    Fixed Log problem

--- foxtrotdoorpanelmobileapp/src/main/java/tu/foxtrot/foxtrotdoorpanelmobileapp/Settings.java
+++ foxtrotdoorpanelmobileapp/src/main/java/tu/foxtrot/foxtrotdoorpanelmobileapp/Settings.java
@@ -8,6 +8,7 @@ import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
