dbf1655520f864ed6425ea520ef876adc7834e90
mister025 <mister.0025@yandex.ru>
2019-02-07 23:16:23 +0100

    Okay, it works now == Once you log in, your messages are pulled

--- server/src/controllers/messages/get-worker-messages.js
+++ server/src/controllers/messages/get-worker-messages.js
@@ -1,4 +1,5 @@
 module.exports = async ctx => {
+    // magic
     console.log('Got request for messages');
     const worker = await ctx.req.user;
     ctx.body = await ctx.messageModel.getBy({workerId: worker.id});
